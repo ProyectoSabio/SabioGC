17613e6e06f47b0fd99c896288dd1492efa4e63f
Pablo <pabloleonpsico@gmail.com>
2018-01-16 12:14:28 +0100

    update

--- sabiogc/funciones/cerrarSesion.php
+++ sabiogc/funciones/cerrarSesion.php
@@ -1,6 +1,8 @@
 <?php
+	ob_start();
 	session_start();
 	unset($_SESSION);
 	session_destroy();
 	session_regenerate_id(true);
 	header("Location: ../index.php");
+	ob_end_flush();
--- trivial/class/configDB.php
+++ trivial/class/configDB.php
@@ -1,7 +1,7 @@
 <?php
 	define('HOST', 'localhost');
-	define('USER', 'sabiogc'); //Crear usuario para la base de datos
-	define('PASS', 'sabiogc');
-	define('DATABASE', 'sabiogc');
+	define('USER', 'root'); //Crear usuario para la base de datos
+	define('PASS', 'serotonina');
+	define('DATABASE', 'sabio');
 	define('PORT', '3306');
 ?>
